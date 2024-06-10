53192f19bb8933da924b9e830fdeec93337888bb
Your Name <you@example.com>
2024-06-11 00:50:48 +0200

    fix api url

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
 
+
 const app = express();
+app.use(express.static('dist'))
 app.use(cors())
 
 const PORT = process.env.PORT || 3001
